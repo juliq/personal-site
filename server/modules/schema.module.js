6b1814c56c8f57de02d85604f4df1b70652c8a31
Monica Wheeler <monica.c.wheeler@gmail.com>
2018-01-29 10:20:38 -0600

    Remove the schema in schema.module

--- server/modules/schema.module.js
+++ server/modules/schema.module.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 let SCHEMANAMEHERE = new mongoose.Schema(
     {
-        url: {type: String, unique: true},
-        name: {type: String},
-        title: {type: String, default: false},
-        favorited: {type: Boolean, default: false}
+        
     }   
 )
 
