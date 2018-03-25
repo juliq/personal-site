7bb1c97ccab6c5de85939835bf9a4877f00aaccc
Monica Wheeler <monica.c.wheeler@gmail.com>
2018-03-25 09:28:29 -0500

    Cleanup. Remove unneeded files

--- server/public/scripts/controllers/name.controller.js
+++
@@ -1,9 +0,0 @@
-myApp.controller("CONTROLLERNAME", ['SERVICENAME', function(SERVICENAME){
-	console.log('CONTROLLERNAME loaded');
-	
-	const self = this;
-
-	//Verify service is working and shared with the views
-	self.message = SERVICENAME.message;
-
-}]);
--- server/public/scripts/services/name.service.js
+++
@@ -1,8 +0,0 @@
-myApp.service('SERVICENAME', ['$http', function($http){
-	console.log('SERVICENAME loaded');
-
-	const self = this;
-	
-	self.message = "Hello from the SERVICENAME!";
-
-}]);
